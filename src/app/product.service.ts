70dd0fbb7846ce40c2ab4d50b56fdac4f071f097
Szymon Czechowski <sczechowski@hotmail.com>
2018-06-24 21:31:02 -0400

    step 3.1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
   private _albumUrl : string = "../assets/album.json";
   constructor(private _http: Http) { }
 
-  public getAlbum(id: number) : any {
+  public getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
